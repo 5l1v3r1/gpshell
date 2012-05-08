23c4363568f89ca855b7bb36842364ead6ba43e9
Shane Farmer <shane.farmer@gmail.com>
2012-05-08 00:17:26 -0700

    Inital gpshell project with autotools support

---
+++ src/gpshell.cc
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : gpshell.cpp
+ Author      : Shane Farmer
+ Version     :
+ Copyright   : Copyright © 2012 NullPointer Software. All rights reserved.
+ Description : Hello World in C++,
+ ============================================================================
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	cout << "Hello World" << endl; /* prints Hello World */
+	return 0;
+}
